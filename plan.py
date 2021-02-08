49b469984c1f2dec9c7a89741c8dc8bc6c619b4f
Anna Likholat <anna.likholat@intel.com>
2021-02-08 23:03:44 +0300

    fixes

--- plan.py
+++ plan.py
@@ -10,11 +10,7 @@ from rdkit.Chem import AllChem
 
 import numpy as np
 import argparse
-from tensorflow.python.ops import gen_nn_ops
-
-parser = argparse.ArgumentParser("Select model representation")
-parser.add_argument("--use_openvino", help="Enable OpenVINO optimization", action='store_true')
-args = parser.parse_args()
+from collections import namedtuple
 
 # Load base compounds
 starting_mols = set()
@@ -25,7 +21,7 @@ with open('data/emolecules.smi', 'r') as f:
             smi = molvs.standardize_smiles(smi)
             starting_mols.add(smi)
 
-            if len(starting_mols) == 1:
+            if len(starting_mols) == 10:
                 break
         except Exception as e:
             print('WARNING', e)
@@ -52,81 +48,17 @@ saver.restore(sess, 'model/model.ckpt')
 exp_res = [[],[]]
 rollout_res = []
 
-if args.use_openvino:
-    import subprocess
-    import sys
-    import mo_tf
-    import os
+# Get input nodes shapes
+exp_net_x_shape = int(expansion_net.X.shape[1])
+roll_net_x_shape = int(rollout_net.X.shape[1])
 
-    from collections import namedtuple
-    from openvino.inference_engine import IECore
-    ie = IECore()
+# Openvino networks
+exp_net, exp_exec_net = None, None
+roll_exec_net = None
 
-    # Get input nodes names
-    exp_inp_node_x_name = expansion_net.X.name.split(':')[0]
-    exp_inp_node_k_name = expansion_net.k.name.split(':')[0]
-    roll_inp_node_name = rollout_net.X.name.split(':')[0]
-
-    pb_model_path = 'model.pb'
-    model_xml = 'model.xml'
-    model_bin = 'model.bin'
-
-    # EXPANSION_NET
-    input_node_names = [exp_inp_node_x_name, exp_inp_node_k_name]
-    output_node_names = ['TopKV2']
-
-    # Save frozen graph
-    graph_def = tf.graph_util.convert_variables_to_constants(
-            sess,
-            sess.graph_def,
-            output_node_names)
-
-    with tf.io.gfile.GFile(pb_model_path, 'wb') as f:
-        f.write(graph_def.SerializeToString())
-
-    # Convert to OpenVINO IR
-    subprocess.run(
-        [
-            sys.executable, mo_tf.__file__, '--input_model', pb_model_path,
-            '--input', ','.join(input_node_names), '--input_shape', "[1, 10000],[1]",
-            '--freeze_placeholder_with_value', "Placeholder_2->5"
-        ],
-        check=True)
-
-    exp_net = ie.read_network(model=model_xml, weights=model_bin)
-    exp_exec_net = ie.load_network(network=exp_net, device_name='CPU')
-
-    os.remove(pb_model_path)
-    os.remove(model_xml)
-    os.remove(model_bin)
-
-
-    # ROLLOUT_NET
-    output_node_names = ['ArgMax']
-    
-    # Save frozen graph
-    graph_def = tf.graph_util.convert_variables_to_constants(
-            sess,
-            sess.graph_def,
-            output_node_names)
-
-    with tf.io.gfile.GFile(pb_model_path, 'wb') as f:
-        f.write(graph_def.SerializeToString())
-
-    # Convert to OpenVINO IR
-    subprocess.run(
-        [
-            sys.executable, mo_tf.__file__, '--input_model', pb_model_path,
-            '--input_shape', "[1, 8912]",
-        ],
-        check=True)
-
-    roll_net = ie.read_network(model_xml, model_bin)
-    roll_exec_net = ie.load_network(roll_net, 'CPU')
-
-    os.remove(pb_model_path)
-    os.remove(model_xml)
-    os.remove(model_bin)
+# Input nodes names
+exp_inp_node_x_name, exp_inp_node_k_name = None, None
+roll_inp_node_name = None
 
 
 def transform(mol, rule):
@@ -143,7 +75,7 @@ def transform(mol, rule):
     return reactants
 
 
-def expansion(node):
+def expansion(node, use_openvino=False):
     """Try expanding each molecule in the current state
     to possible reactants"""
 
@@ -153,17 +85,14 @@ def expansion(node):
     # Convert mols to format for prediction
     # If the mol is in the starting set, ignore
     mols = [mol for mol in mols if mol not in starting_mols]
-    fprs = policies.fingerprint_mols(mols, expansion_net.X.shape[1])
+    fprs = policies.fingerprint_mols(mols, exp_net_x_shape)
 
-    if not args.use_openvino:
+    if not use_openvino:
         # Predict applicable rules
         preds = sess.run(expansion_net.pred, feed_dict={
             expansion_net.X: fprs,
             expansion_net.k: 5
         })
-
-        # indices = preds.indices[0]
-        # values = preds.values[0]
 
         exp_res[0].append(preds.indices[0])
         exp_res[1].append(preds.values[0])
@@ -214,7 +143,7 @@ def expansion(node):
     return children
 
 
-def rollout(node, max_depth=200):
+def rollout(node, max_depth=200, use_openvino=False):
     cur = node
     for _ in range(max_depth):
         if cur.is_terminal:
@@ -223,9 +152,9 @@ def rollout(node, max_depth=200):
         # Select a random mol (that's not a starting mol)
         mols = [mol for mol in cur.state if mol not in starting_mols]
         mol = random.choice(mols)
-        fprs = policies.fingerprint_mols([mol], rollout_net.X.shape[1])
+        fprs = policies.fingerprint_mols([mol], roll_net_x_shape)
 
-        if not args.use_openvino:
+        if not use_openvino:
             # Predict applicable rules
             preds = sess.run(rollout_net.pred_op, feed_dict={
                 rollout_net.X: fprs,
@@ -268,13 +197,13 @@ def rollout(node, max_depth=200):
     return 1.
 
 
-def plan(target_mol):
+def plan(target_mol, use_openvino=False):
     """Generate a synthesis plan for a target molecule (in SMILES form).
     If a path is found, returns a list of (action, state) tuples.
     If a path is not found, returns None."""
     root = Node(state={target_mol})
 
-    path = mcts(root, expansion, rollout, iterations=2000, max_depth=200)
+    path = mcts(root, expansion, rollout, iterations=2000, max_depth=200, use_openvino=use_openvino)
     if path is None:
         print('No synthesis path found. Try increasing `iterations` or `max_depth`.')
     else:
@@ -283,12 +212,83 @@ def plan(target_mol):
     return path
 
 
+def convert_to_ir(sess, input_shape, output_node_names, input_node_names=None, placeholder_value=None):
+    import subprocess
+    import sys
+    import mo_tf
+    import os
+
+    from openvino.inference_engine import IECore
+    ie = IECore()
+
+    pb_model_path = 'model.pb'
+    model_xml = 'model.xml'
+    model_bin = 'model.bin'
+
+    # Save frozen graph
+    graph_def = tf.graph_util.convert_variables_to_constants(
+            sess,
+            sess.graph_def,
+            output_node_names)
+
+    with tf.io.gfile.GFile(pb_model_path, 'wb') as f:
+        f.write(graph_def.SerializeToString())
+
+    inp_shape = [] 
+
+    for shape in input_shape:
+        inp_shape.append([1])
+        if shape > 0:
+            inp_shape[-1].append(shape)
+
+    inp_shape = str(inp_shape)[1:-1]
+    convert_script = [sys.executable, mo_tf.__file__, 
+                    '--input_model', pb_model_path, '--input_shape', inp_shape]
+
+    if input_node_names:
+        convert_script.append('--input')
+        convert_script.append(','.join(input_node_names))
+
+    if placeholder_value:
+        convert_script.append('--freeze_placeholder_with_value'),
+        convert_script.append(placeholder_value)
+
+    # Convert to OpenVINO IR
+    subprocess.run(convert_script, check=True)
+
+    net = ie.read_network(model=model_xml, weights=model_bin)
+    exec_net = ie.load_network(network=net, device_name='CPU')
+
+    os.remove(pb_model_path)
+    os.remove(model_xml)
+    os.remove(model_bin)
+
+    return net, exec_net
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Select model representation")
+    parser.add_argument("--use_openvino", help="Enable OpenVINO optimization", action='store_true')
+    args = parser.parse_args()
+
+    if args.use_openvino:
+        # Get input nodes names
+        exp_inp_node_x_name = expansion_net.X.name.split(':')[0]
+        exp_inp_node_k_name = expansion_net.k.name.split(':')[0]
+        roll_inp_node_name = rollout_net.X.name.split(':')[0]
+
+        exp_input_nodes = [exp_inp_node_x_name, exp_inp_node_k_name]
+        exp_output_nodes = ['TopKV2']
+        roll_output_nodes = ['ArgMax']
+
+        exp_net, exp_exec_net = convert_to_ir(sess, [exp_net_x_shape, 0], exp_output_nodes, exp_input_nodes, "Placeholder_2->5")
+        _, roll_exec_net = convert_to_ir(sess, [roll_net_x_shape], roll_output_nodes)
+
     # target_mol = '[H][C@@]12OC3=C(O)C=CC4=C3[C@@]11CCN(C)[C@]([H])(C4)[C@]1([H])C=C[C@@H]2O'
     target_mol = 'CC(=O)NC1=CC=C(O)C=C1'
     root = Node(state={target_mol})
-    path = plan(target_mol)
-
+    path = plan(target_mol, use_openvino=args.use_openvino)
+    
     if not args.use_openvino:
         np.save('expansion_preds_tf', exp_res)
         np.save('rollout_preds_tf', rollout_res)
